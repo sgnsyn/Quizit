3bb389dd53e29dc2137fc25e4f1a20c2fcc62da5
sgnsyn <sgnsyn45@gmail.com>
2025-08-27 10:16:40 +0200

    file tree completed

--- script.js
+++ script.js
@@ -22,3 +22,58 @@ const directory = {
     { type: "file", name: "welcome" },
   ],
 };
+
+document.querySelectorAll(".filetree .expand-collapse").forEach((button) => {
+  button.addEventListener("click", () => {
+    const parentDir = button.closest(".current-dir");
+    if (parentDir) {
+      const dir = parentDir.parentElement;
+      const childDir = dir.querySelector(".child-dir");
+      if (childDir) {
+        childDir.classList.toggle("collapsed");
+      }
+      const icon = button.querySelector(".exp-icon");
+      if (icon) {
+        icon.classList.toggle("collapsed");
+      }
+    }
+  });
+});
+
+const nav = document.querySelector("nav");
+const main = document.querySelector("main");
+const resizer = document.querySelector(".resizer");
+const filetree = document.querySelector(".filetree");
+
+const resize = (e) => {
+  const newNavWidth = e.clientX;
+  nav.style.width = `${newNavWidth}px`;
+  main.style.width = `calc(100vw - ${newNavWidth}px)`;
+  checkOverflow();
+};
+
+resizer.addEventListener("mousedown", (e) => {
+  e.preventDefault();
+  document.addEventListener("mousemove", resize);
+  document.addEventListener("mouseup", () => {
+    document.removeEventListener("mousemove", resize);
+  });
+});
+
+document.querySelectorAll(".filetree .expand-collapse").forEach((button) => {
+  button.addEventListener("click", () => {
+    const parentDir = button.closest(".current-dir");
+    if (parentDir) {
+      const dir = parentDir.parentElement;
+      const childDir = dir.querySelector(".child-dir");
+      if (childDir) {
+        childDir.classList.toggle("collapsed");
+      }
+      const icon = button.querySelector(".exp-icon");
+      if (icon) {
+        icon.classList.toggle("collapsed");
+      }
+      checkOverflow();
+    }
+  });
+});
